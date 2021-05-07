09556e4466c1436ecb9d616a2dffb3e7a80a05d4
Egogorka <bike034@mail.ru>
2021-05-07 11:30:34 +0300

    deleted unnecessary defines

--- main.cpp
+++ main.cpp
@@ -6,18 +6,6 @@ using namespace sf;
 #include "headers/Table.h"
 #include "headers/App.h"
 
-#define RESOLUTION_HEIGHT 600
-#define RESOLUTION_WIDTH 800
-
-#define RESOLUTION Vector2f(RESOLUTION_WIDTH, RESOLUTION_HEIGHT)
-
-#define CENTER_HEIGHT RESOLUTION_HEIGHT/2
-#define CENTER_WIDTH RESOLUTION_WIDTH/2
-
-#define CENTER RESOLUTION/2.f
-
-#define TABLE_SIZE 10
-
 int main() {
     App app;
     app.run();
